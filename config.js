a990c8a1cd409e76975ff1b0cdc63a7d16eb5e13
borys-duda <borys.duda0928@gmail.com>
2016-08-27 08:07:54 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+  twilioAccountSID:  'AC300adeb6b9dfd34b15f07a6e126fe460',
+  twilioAuthToken: 'aa9188c0610679ca7976104e1f011743',
+
+  mysqlHost: "aaaczenxl60o21.crjwypp8qyef.us-east-1.rds.amazonaws.com",
+  mysqlUser: "rmistry75",
+  mysqlPassword: "Bounce2016",
+  mysqlDatabase: "ebdb",
+
+  StripeOAuthClientID: "ca_8zXcdeJDJXH5p7NIjIS1H9imXDewtAeF",
+  StripeOAuthClientSecret: "sk_test_gXuZmNJG89S7ULTRXejRAO5x"
+};
+module.exports = config;
+
+
