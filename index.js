473ad5e6e294b589fa80f3afb3bedb86bd49e754
borys-duda <borys.duda0928@gmail.com>
2016-08-29 09:24:11 +0200

    ACL

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ https.createServer({
   cert: fs.readFileSync('cert.pem')
 }, app).listen(443);
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
+
 // Twilio SMS client 
 var twilioClient = require('twilio')(config.twilioAccountSID, config.twilioAuthToken); 
  
